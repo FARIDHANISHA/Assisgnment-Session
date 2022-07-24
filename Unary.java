cca92ac286b8d4c240280b7f38d652c7febd6c40
FARIDHANISHA <108788056+FARIDHANISHA@users.noreply.github.com>
2022-07-24 07:28:39 -0700

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,23 @@
+class Unary
+{
+    public  static void main (String []args)
+    {
+        int i=10;
+		int j=11;
+		int k=6;
+	
+		   System.out.println("increment of i" +" " + ++i);
+		    System.out.println("increment of j" +" "+ --j);
+			 System.out.println("value of k is "+" " +k );
+	}
+	
+}
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
